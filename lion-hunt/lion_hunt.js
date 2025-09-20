a248f9e8e2beef5199e706aaac96b4d0d79ff227
Rafiya <157607173+Rafiya1207@users.noreply.github.com>
2025-09-20 19:20:10 +0530

    modify condition for length 3 string

--- lion-hunt/lion_hunt.js
+++ lion-hunt/lion_hunt.js
@@ -1,8 +1,20 @@
-const savannahMap = "Z ";
+const savannahMap = "Z L";
 let distance;
 
-if (savannahMap[0] !== " " && savannahMap[1] !== " ") {
-    distance = 0;
+if(savannahMap[0] !== " ") {
+
+    if(savannahMap[1] !== " ") {
+        distance = 0;
+    } else {
+        distance = -1;
+    }
+
+    if(savannahMap[2] !== " ") {
+        distance = 1;
+    } else {
+        distance = -1;
+    }
+    
 } else {
     distance = -1;
 }
