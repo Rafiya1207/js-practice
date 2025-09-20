5e9bd94fc06374a2e35e2d38eb4c6311ddaaa60d
Rafiya <157607173+Rafiya1207@users.noreply.github.com>
2025-09-20 19:01:21 +0530

    update condition to calculate distance 0

--- lion-hunt/lion_hunt.js
+++ lion-hunt/lion_hunt.js
@@ -1,8 +1,10 @@
-const savannahMap = "LZ";
+const savannahMap = "ZL";
 let distance;
 
-if (savannahMap[0] === "L" && savannahMap[1] === "Z") {
-    distance = 0;
+if (savannahMap[0] === "L" || savannahMap[0] === "Z") {
+    if (savannahMap[1] === "L" || savannahMap[1] === "Z") {
+        distance = 0;
+    }
 }
 
 console.log(distance);
