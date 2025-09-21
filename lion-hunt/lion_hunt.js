743cf87202cfa9a67cd53580e6919458af5171f5
Rafiya <157607173+Rafiya1207@users.noreply.github.com>
2025-09-21 14:08:58 +0530

    Update lion_hunt.js

--- lion-hunt/lion_hunt.js
+++ lion-hunt/lion_hunt.js
@@ -1,16 +1,20 @@
-const savannahMap = "L  ZL Z";
+const savannahMap = "L     Z";
 let distance = -1;
-
-let minDistance = 0;
+let minDistance;
 
 for(let index = 1; index < savannahMap.length; index++) {
-    
     distance += 1;
 
     if(savannahMap[index] !== " ") {
+        
+        if (minDistance === undefined) {
+            minDistance = distance;
+        }
+        
         minDistance = distance < minDistance ? distance : minDistance;
         distance = -1;
     }
 
 }
+
 console.log(minDistance);
