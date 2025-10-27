5228fa84feb0278daf5a6ad29158d7a02a6d58a5
Rafiya1207 <rafiya1207@gmail.com>
2025-10-27 10:02:43 +0530

    create functions to encode number and string

--- bencode-cipher/bencode_cipher.js
+++ bencode-cipher/bencode_cipher.js
@@ -1,5 +1,18 @@
+function encodeNumberData(numberData) {
+	return 'i' + numberData + 'e';
+}
+
+function encodeStringData(stringData) {
+	return stringData.length + ':' + stringData;
+}
+
 function encode(data) {
-	return 'i' + data + 'e';
+	const type = typeof (data);
+
+	switch (type) {
+		case 'number': return encodeNumberData(data);
+		case 'string': return encodeStringData(data);
+	}
 }
 
 function composeMsg(description, data, expected, received) {
@@ -29,6 +42,10 @@ function testAll() {
 	testEncode('data type is number', 123, 'i123e');
 	testEncode('data is negative number', -42, 'i-42e');
 	testEncode('data is zero', 0, 'i0e');
+	testEncode('data is a word', "hello", "5:hello");
+	testEncode('data is empty string', "", "0:");
+	testEncode('data is text', "hello world", "11:hello world");
+	testEncode('special charcters', "special!@#$chars", "16:special!@#$chars");
 }
 
 testAll();
