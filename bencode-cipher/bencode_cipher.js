89d37da70efef89330fe42c99ab95249f1aaf150
Rafiya1207 <rafiya1207@gmail.com>
2025-10-27 08:42:49 +0530

    add test function encode()

---
+++ bencode-cipher/bencode_cipher.js
@@ -0,0 +1,32 @@
+function encode(data) {
+
+}
+
+function composeMsg(description, data, expected, received) {
+	const spaces = ' '.repeat(5);
+	const dashes = '-'.repeat(30);
+	const isPassed = expected === received;
+	const symbol = isPassed ? '✅' : '❌';
+
+	let message = `${symbol} | ${description}\n`;
+
+	if (!isPassed) {
+		message += `${dashes}\n`;
+		message += `${spaces}data: ${data}\n`;
+		message += `${spaces}expected: ${expected}\n`;
+		message += `${spaces}receivec: ${received}\n`;
+		message += `${dashes}\n`;
+	}
+	return message;
+}
+
+function testEncode(description, data, expected) {
+	const received = encode(data);
+	console.log(composeMsg(description, data, expected, received));
+}
+
+function testAll() {
+	testEncode('data type is number', 123, 'i123e');
+}
+
+testAll();
